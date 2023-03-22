4e1d492b677183efd2a3e987e0c83e4eb81c9de5
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-03-21 23:19:29 -0400

    Completed exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, args) {
+    // remove the selected items from the array
+    for (let i = 1; i < arguments.length; i++) {
+        const index = arr.indexOf(arguments[i]);
+        // if the item is in the array, remove it
+        if (index > -1)
+            arr.splice(index, 1);
+    }
+    
+    // return the modified array
+    return arr;
 };
 
 // Do not edit below this line
