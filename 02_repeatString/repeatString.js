01e34afa42abc76629e64a5c9e50a4dcf137b09a
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-03-21 23:20:22 -0400

    Completed exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0)
+  return "ERROR"
+  if (num === 0){
+  return ''
+  }
+  for (let i = 0; i < num; i++) {
+  return str.repeat(num);
+  }
 };
 
 // Do not edit below this line
