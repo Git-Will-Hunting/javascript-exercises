3d251c433be1906480b8bb3091a3f7e490c4343a
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-07 13:05:10 -0400

    Completed calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,66 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  if(arr.length === 0) {
+    return 0;
+  }
+  // check if the array contains only numbers
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') {
+      return NaN;
+    }
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  multiple = 1;
+  // check if the arguments are numbers
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] !== 'number') {
+      return NaN;
+    }
+    multiple *= args[i];
+  }
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 ** num2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if (num1 === 0) {
+    return 1;
+  } else {
+    return num1 * factorial(num1 - 1);
+  }
 };
 
 // Do not edit below this line
