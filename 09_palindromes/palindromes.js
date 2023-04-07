73778007df81e3e72531e43973bbdc60d44ae33e
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-07 15:01:25 -0400

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    // check if the argument is a string
+    if (typeof arr !== 'string') {
+        return false;
+    }
+    // remove all non-alphanumeric characters
+    let str = arr.replace(/[^a-zA-Z0-9]/g, '');
+    // convert the string to lowercase
+    str = str.toLowerCase();
+    // reverse the string
+    let reverseStr = str.split('').reverse().join('');
+    // check if the string is a palindrome
+    if (str === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
