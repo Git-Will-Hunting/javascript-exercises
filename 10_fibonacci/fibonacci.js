450e0c1fd4d4d0ae76f27f12be16487d669850c2
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-07 15:23:17 -0400

    Completed fibonacci with memoization

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num, memo = {}) {
+    // validate input
+    if (num < 0) return "OOPS"
+    // convert string to number
+    num = Number(num)    
+    // fibonacci with memoization for optimization
+    if (num in memo) return memo[num]
+    if (num <= 2) return 1
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+    return memo[num]
 };
 
 // Do not edit below this line
